94ed16a82d9f5ea10ffd1a224e094c7a947ad965
vaneev nikita <vaneev.nikita2014@yandex.ru>
2017-03-05 21:46:58 +0300

    izm

--- src/add.cpp
+++ src/add.cpp
@@ -1,32 +1,30 @@
 #include<add.h>
 CNode* find(CNode* root, int v) {
-	while (root) {
-		if ((root->value > v) && (root->right != 0)) {
-			root = root->right;
-		}
-		else if ((root->value < v) && (root->left != 0)) {
-			root = root->left;
-		}
-		else {
+	while (root) {	
+		if ((root->value > v) && (root->right != 0)) {	
+			root = root->right;	
+		} else if ((root->value < v) && (root->left != 0)) {	
+			root = root->left;		
+		} else {
 			return root;
-		}
-	}
-	return root;
-}
+		}	
+	}	
+	return root;	
+}	
 
-void PrintTree(CNode** root) {
-	std::queue<CNode**> q;
-	q.push(root);
-	CNode** tmp;
+void PrintTree(CNode** root) {	
+	std::queue<CNode**> q;	
+	q.push(root);	
+	CNode** tmp;	
 
-	while (!q.empty()) {
-		tmp = q.front();
-		q.pop();
+	while (!q.empty()) {	
+		tmp = q.front();	
+		q.pop();	
 
-		if ((*tmp)->left)
-			q.push(&(*tmp)->left);
-		if ((*tmp)->right)
-			q.push(&(*tmp)->right);
+		if ((*tmp)->left)	
+			q.push(&(*tmp)->left);	
+		if ((*tmp)->right)	
+			q.push(&(*tmp)->right);	
 
 		std::cout << tmp << std::endl << "value:" << (*tmp)->value << std::endl <<
 			"&left: " << &(*tmp)->left << std::endl << "&right: " << &(*tmp)->right
--- test/test.cpp
+++ test/test.cpp
@@ -2,27 +2,27 @@
 #include "add.h"
 
 TEST(find, NullTree) {
-	CNode* tree = 0;
-	CNode* res = find(tree, 1);
-	EXPECT_EQ(res, tree);
+	CNode* tree = 0;	
+	CNode* res = find(tree, 1);	
+	EXPECT_EQ(res, tree);	
 }
 
 TEST(find, left) {
-	CNode* tree = new CNode(5);
-	tree->left = new CNode(6);
-	tree->right = new CNode(4);
-	tree->right->right = new CNode(3);
-	CNode* res = find(tree, 6);
-	EXPECT_EQ(6, res->value);
+	CNode* tree = new CNode(5);	
+	tree->left = new CNode(6);	
+	tree->right = new CNode(4);	
+	tree->right->right = new CNode(3);	
+	CNode* res = find(tree, 6);	
+	EXPECT_EQ(6, res->value);	
 }
 
 TEST(find, right) {
-	CNode* tree = new CNode(5);
-	tree->left = new CNode(6);
-	tree->right = new CNode(4);
-	tree->right->right = new CNode(3);
-	CNode* res = find(tree, 3);
-	EXPECT_EQ(3, res->value);
+	CNode* tree = new CNode(5);	
+	tree->left = new CNode(6);	
+	tree->right = new CNode(4);	
+	tree->right->right = new CNode(3);	
+	CNode* res = find(tree, 3);	
+	EXPECT_EQ(3, res->value);	
 }
 
 int main(int ac, char* av[]) {
