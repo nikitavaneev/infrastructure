1aaa2cda03929e633231b03fba83a99d06d843ed
vaneev nikita <vaneev.nikita2014@yandex.ru>
2017-03-05 22:20:19 +0300

    new2

--- include/add.h
+++ include/add.h
@@ -1,15 +1,15 @@
-#ifndef INCLUDE_ADD_H_ 
-#define INCLUDE_ADD_H_ 
-#include<iostream> 
-#include<queue> 
-using std::cout; 
-using std::endl; 
-struct CNode { 
-	int value; 
-	CNode* left; 	
-	CNode* right; 	
-	explicit CNode(int v) { value = v; left = 0; right = 0; } 	
-}; 
-CNode* find(CNode* root, int v); 
-void PrintTree(CNode** root); 
-#endif  // INCLUDE_ADD_H_ 	
+#ifndef INCLUDE_ADD_H_
+#define INCLUDE_ADD_H_
+#include<iostream>
+#include<queue>
+using std::cout;
+using std::endl;
+struct CNode {
+    int value;
+    CNode* left;
+    CNode* right;
+    explicit CNode(int v) { value = v; left = 0; right = 0; }
+};
+CNode* find(CNode* root, int v);
+void PrintTree(CNode** root);
+#endif  // INCLUDE_ADD_H_
