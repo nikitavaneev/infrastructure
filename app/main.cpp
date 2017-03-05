6f471493d61f691fe5906596839af47385ab1255
vaneev nikita <vaneev.nikita2014@yandex.ru>
2017-03-05 22:44:30 +0300

    finish

--- app/main.cpp
+++ app/main.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 int main() {
     CNode* tree1 = 0;
-	PrintTree(&tree1);
+    PrintTree(&tree1);
     CNode* tree = new CNode(5);
     tree->left = new CNode(6);
     tree->right = new CNode(4);
     tree->right->right = new CNode(3);
-	PrintTree(&tree);
+    PrintTree(&tree);
 }
 
