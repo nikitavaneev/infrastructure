0117af418af5fb094f35cea3467b30776679444a
vaneev nikita <vaneev.nikita2014@yandex.ru>
2017-03-05 22:42:38 +0300

    ttt

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,12 @@
 #include "add.h"
 #include <iostream>
-
 int main() {
-  std::cout << "2 + 2 = " << add(2, 2) << std::endl;
+    CNode* tree1 = 0;
+	PrintTree(&tree1);
+    CNode* tree = new CNode(5);
+    tree->left = new CNode(6);
+    tree->right = new CNode(4);
+    tree->right->right = new CNode(3);
+	PrintTree(&tree);
 }
+
